61da0c293f3a66e98c134191e8b02f068b6b4eb9
Humayra-Amin <humaema4576@gmail.com>
2024-06-22 18:53:04 +0600

    banner added

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,3 +1,4 @@
+import Banner from "../Banner/Banner";
 import Footer from "../Footer/Footer";
 import Navbar from "../Navbar/Navbar";
 
@@ -6,6 +7,7 @@ const HomePage = () => {
     return (
         <div className="bg-gradient-to-b from-yellow-200 to-yellow-50">
             <Navbar></Navbar>
+            <Banner></Banner>
             <Footer></Footer>
         </div>
     );
