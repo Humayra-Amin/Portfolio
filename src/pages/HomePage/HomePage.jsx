c3987e1251d4347ae4fadd65959f0f2b87567868
Humayra-Amin <humaema4576@gmail.com>
2024-06-22 18:47:31 +0600

    homepage added

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,14 @@
+import About from "../About/About";
+import Banner from "../Banner/Banner";
+import Footer from "../Footer/Footer";
+
+
+const HomePage = () => {
+    return (
+        <div className="bg-gradient-to-b from-yellow-200 to-yellow-50">
+
+        </div>
+    );
+};
+
+export default HomePage;
